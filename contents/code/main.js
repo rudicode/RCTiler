07474bdcae65c0f3bf20100fe10ae2268bffd875
Rudi Luzar <rudi@rudicode.com>
2021-07-17 00:48:17 -0400

    version 0.1 completed

--- contents/code/main.js
+++ contents/code/main.js
@@ -29,7 +29,33 @@ function getGeometry() {
     screenGeometry = workspace.clientArea(KWin.MaximizeArea, client);
 }
 
+function move(newX, newY) {
+    if (!client.moveable) {
+        return;
+    }
+    client.move = true;
+    client.geometry = {
+        x: newX,
+        y: newY,
+        width: client.geometry.width,
+        height: client.geometry.height
+    };
+    client.move = false;
+}
 
+function resize(newWidth, newHeight) {
+    if (!client.resizeable) {
+        return;
+    }
+    client.resize = true;
+    client.geometry = {
+        x: client.geometry.x,
+        y: client.geometry.y,
+        width: newWidth,
+        height: newHeight
+    };
+    client.resize = false;
+}
 
 //
 // Tiling Functions
@@ -38,216 +64,105 @@ function getGeometry() {
 function tileBotLeft() {
     getGeometry();
 
-    if (!client.moveable) { return; }
-
     var newWidth = screenGeometry.width / 2;
     var newHeight = screenGeometry.height / 2;
 
-    if (client.resizeable) {
-        client.geometry = {
-            x: screenGeometry.x + xMargin,
-            y: screenGeometry.y + newHeight,
-            width: newWidth - xMargin,
-            height: newHeight
-        };
-    } else {
-        client.geometry = {
-            x: screenGeometry.x + xMargin,
-            y: screenGeometry.y + newHeight,
-            width: client.width,
-            height: client.height
-        };
-    }
+    move(screenGeometry.x + xMargin, screenGeometry.y + newHeight);
+    resize(newWidth - xMargin, newHeight);
 }
 
 function tileBotRight() {
     getGeometry();
 
-    if (!client.moveable) { return; }
-
     var newWidth = screenGeometry.width / 2;
     var newHeight = screenGeometry.height / 2;
 
-    if (client.resizeable) {
-        client.geometry = {
-            x: screenGeometry.x + newWidth,
-            y: screenGeometry.y + newHeight,
-            width: newWidth - xMargin,
-            height: newHeight
-        };
-    } else {
-        client.geometry = {
-            x: screenGeometry.x + newWidth,
-            y: screenGeometry.y + newHeight,
-            width: client.width,
-            height: client.height
-        };
-    }
+    move(screenGeometry.x + newWidth, screenGeometry.y + newHeight);
+    resize(newWidth - xMargin, newHeight);
 }
 
 function tileTopLeft() {
     getGeometry();
 
-    if (!client.moveable) { return; }
-
     var newWidth = screenGeometry.width / 2;
     var newHeight = screenGeometry.height / 2;
 
-    if (client.resizeable) {
-        client.geometry = {
-            x: screenGeometry.x + xMargin,
-            y: screenGeometry.y,
-            width: newWidth - xMargin,
-            height: newHeight
-        };
-    } else {
-        client.geometry = {
-            x: screenGeometry.x + xMargin,
-            y: screenGeometry.y,
-            width: client.width,
-            height: client.height
-        };
-    }
+    move(screenGeometry.x + xMargin, screenGeometry.y);
+    resize(newWidth - xMargin, newHeight);
 }
 
 function tileTopRight() {
     getGeometry();
 
-    if (!client.moveable) { return; }
-
     var newWidth = screenGeometry.width / 2;
     var newHeight = screenGeometry.height / 2;
 
-    if (client.resizeable) {
-        client.geometry = {
-            x: screenGeometry.x + newWidth,
-            y: screenGeometry.y,
-            width: newWidth - xMargin,
-            height: newHeight
-        };
-    } else {
-        client.geometry = {
-            x: screenGeometry.x + newWidth,
-            y: screenGeometry.y,
-            width: client.width,
-            height: client.height
-        };
-    }
+    move(screenGeometry.x + newWidth, screenGeometry.y);
+    resize(newWidth - xMargin, newHeight);
 }
 
 function tileCenterLandscape() {
     getGeometry();
 
-    if (!client.moveable) { return; }
+    var clientScaleX = 0.6;
+    var clientScaleY = 0.70;
+    resize(screenGeometry.width * clientScaleX, screenGeometry.height * clientScaleY);
 
-    if (client.resizeable) {
-        // center window and scale to % of screenGeometry
-        var clientScaleX = 0.6;
-        var clientScaleY = 0.6;
-        client.geometry = {
-          x: screenGeometry.x + (screenGeometry.width - screenGeometry.width * clientScaleX) / 2,
-          y: screenGeometry.y + (screenGeometry.height - screenGeometry.height * clientScaleY) / 2,
-          width: screenGeometry.width * clientScaleX,
-          height: screenGeometry.height * clientScaleY
-        };
-      } else {
-        // only center window
-        client.geometry = {
-          x: screenGeometry.x + (screenGeometry.width - client.width) / 2,
-          y: screenGeometry.y + (screenGeometry.height - client.height) / 2,
-          width: client.width,
-          height: client.height
-        };
-      }
+    var newX = screenGeometry.x + (screenGeometry.width - client.width) / 2;
+    var newY = screenGeometry.y + (screenGeometry.height - client.height) / 2;
+    move(newX, newY);
 }
 
 function tileCenterPortrait() {
     getGeometry();
+    // var clientScaleX = 0.34;
+    // var clientScaleY = 0.60;
+    var newWidth = (screenGeometry.width - xMargin * 2) / 3;
+    var newHeight = screenGeometry.height * 0.60;
+    // resize(screenGeometry.width * clientScaleX, screenGeometry.height * clientScaleY);
+    resize(newWidth, newHeight);
 
-    if (!client.moveable) { return; }
-
-    if (client.resizeable) {
-      // center window and scale to % of screenGeometry
-      var clientScaleX = 0.30;
-      var clientScaleY = 0.70;
-      client.geometry = {
-        x: screenGeometry.x + (screenGeometry.width - screenGeometry.width * clientScaleX) / 2,
-        y: screenGeometry.y + (screenGeometry.height - screenGeometry.height * clientScaleY) / 2,
-        width: screenGeometry.width * clientScaleX,
-        height: screenGeometry.height * clientScaleY
-      };
-    } else {
-      // only center window
-      client.geometry = {
-        x: screenGeometry.x + (screenGeometry.width - client.width) / 2,
-        y: screenGeometry.y + (screenGeometry.height - client.height) / 2,
-        width: client.width,
-        height: client.height
-      };
-    }
+    var newX = screenGeometry.x + (screenGeometry.width - client.width) / 2;
+    var newY = screenGeometry.y + (screenGeometry.height - client.height) / 2;
+    move(newX, newY);
 }
 
 function tileRightOfCenter() {
     getGeometry();
+    var clientScaleX = 0.34;
+    var clientScaleY = 0.60;
+    var newWidth = screenGeometry.width * clientScaleX;
+    resize(newWidth, screenGeometry.height * clientScaleY);
 
-    if (!client.moveable) { return; }
-
-    if (client.resizeable) {
-      var clientScaleX = 0.30;
-      var clientScaleY = 0.70;
-      var newX = screenGeometry.x + screenGeometry.width / 2;
-      if (newX == client.geometry.x) {
-        newX = screenGeometry.width - client.width;
-      }
-      client.geometry = {
-        x: newX,
-        y: screenGeometry.y + (screenGeometry.height - screenGeometry.height * clientScaleY) / 2,
-        width: screenGeometry.width * clientScaleX,
-        height: screenGeometry.height * clientScaleY
-      };
-    } else {
-      // only Right of Center window
-      client.geometry = {
-        x: screenGeometry.x + screenGeometry.width / 2,
-        y: screenGeometry.y + (screenGeometry.height - client.height) / 2,
-        width: client.width,
-        height: client.height
-      };
+    var centerX = screenGeometry.x + (screenGeometry.width - client.width) / 2;
+    var newX = centerX + client.width
+    var newY = screenGeometry.y + (screenGeometry.height - client.height) / 2;
+    if ((newX == client.geometry.x) ) {
+            newX = screenGeometry.width - client.width - xMargin;
     }
+    move(newX, newY);
 }
 
 function tileLeftOfCenter() {
     getGeometry();
+    var clientScaleX = 0.34;
+    var clientScaleY = 0.60;
+    var newWidth = screenGeometry.width * clientScaleX;
+    resize(newWidth, screenGeometry.height * clientScaleY);
 
-    if (!client.moveable) { return; }
-
-    if (client.resizeable) {
-      var clientScaleX = 0.30;
-      var clientScaleY = 0.70;
-      var newX = (screenGeometry.x + (screenGeometry.width / 2)) - client.width;
-      if (newX == client.geometry.x) {
-        newX = screenGeometry.x;
-      }
-      client.geometry = {
-        x: newX,
-        y: screenGeometry.y + (screenGeometry.height - screenGeometry.height * clientScaleY) / 2,
-        width: screenGeometry.width * clientScaleX,
-        height: screenGeometry.height * clientScaleY
-      };
-    } else {
-      // only Left of Center window
-      client.geometry = {
-        x: screenGeometry.x + (screenGeometry.width / 2) - client.width,
-        y: screenGeometry.y + (screenGeometry.height - client.height) / 2,
-        width: client.width,
-        height: client.height
-      };
+    var centerX = screenGeometry.x + (screenGeometry.width - client.width) / 2;
+    var newX = centerX - client.width;
+    var newY = screenGeometry.y + (screenGeometry.height - client.height) / 2;
+    if (newX == client.geometry.x) {
+        newX = screenGeometry.x + xMargin;
     }
+    move(newX, newY);
 }
 
 
-
+//
 // Register Shortcuts
+//
 
 registerShortcut("RCTilerBotLeft1", "RCTiler Bot Left", "Meta+Num+1", function () {
     tileBotLeft();
@@ -265,11 +180,11 @@ registerShortcut("RCTilerTopRight9", "RCTiler Top Right", "Meta+Num+9", function
     tileTopRight();
 });
 
-registerShortcut("RCTilerCenterPortrait5", "RCTiler Center Portrait", "Meta+Num+5", function () {
+registerShortcut("RCTilerCenterPortrait5", "RCTiler Center-Portrait", "Meta+Num+5", function () {
     tileCenterPortrait();
 });
 
-registerShortcut("RCTilerCenterLandscape2", "RCTiler Center Landscape", "Meta+Num+2", function () {
+registerShortcut("RCTilerCenterLandscape2", "RCTiler Center-Landscape", "Meta+Num+2", function () {
     tileCenterLandscape();
 });
 
